fa45e7d6d5cd702bd00fa7d356ce2b1c25e738a0
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-07-06 17:12:02 -0300

    Criado Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import LogoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+        <Content>
+            <img src={LogoImg} alt="dt-modey" />
+            <button type="button">Nova transação</button>
+        </Content>
+     
+    </Container>
+  );
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     :root{
        --background: #f0f2f5; 
+       --red: #e52e4d;
+       --blue: #5429cc;
+
+       --blue-light: #6933ff;
+
+       --text-title: #363f5f;
+       --text-body:#969cb3;
+
+       --shape: #ffffff;
     }
 
     *{
