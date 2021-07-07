711679847dfa06478ad8a85831b48fa08f3ea527
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-07-07 17:10:02 -0300

    Adicionado miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-
+import { createServer } from 'miragejs'
 import { GlobalStyle } from "./styles/global";
 
+createServer({
+  routes(){
+    this.namespace='api'
 
+    this.get('/transactions', ()=>{
+      return[
+        {
+          id: 1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposit',
+          categoty: 'Food'
+        }
+      ]
+    })
+  }
+})
 export function App() {
   return (
     <>
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,32 +1,38 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export function TransctionsTable(){
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Desenvolviemento</td>
-                        <td className="deposity">10.000</td>
-                        <td>Desenvolviemento</td>
-                        <td>01/01/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolviemento</td>
-                        <td className="withdraw">10.000</td>
-                        <td>Desenvolviemento</td>
-                        <td>01/01/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    )
+export function TransctionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolviemento</td>
+            <td className="deposity">10.000</td>
+            <td>Desenvolviemento</td>
+            <td>01/01/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolviemento</td>
+            <td className="withdraw">10.000</td>
+            <td>Desenvolviemento</td>
+            <td>01/01/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
