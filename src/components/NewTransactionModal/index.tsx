4d3516562b507ad245a867e4631d513f4f8ff0b5
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-07-08 09:48:10 -0300

    Estilização do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from "react-modal";
+import closeImg from '../../assets/close.svg';
+
+import { Container } from "./styles";
 interface NewTransactionsModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -9,8 +12,23 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionsModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      cadastro
+    <Modal 
+    isOpen={isOpen} 
+    onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
+    >
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="close" />
+      </button>
+      <Container>
+        <h2>Novo Cadastro</h2>
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
